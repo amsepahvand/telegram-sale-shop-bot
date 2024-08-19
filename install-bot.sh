a7272d1d6f0ce984ae32391ba1500f6b409ab5f6
Amir <123931830+amsepahvand@users.noreply.github.com>
2024-08-19 22:30:50 +0330

    Create install-bot.sh

---
+++ install-bot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y git
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+rm get-docker.sh
+
+sudo apt-get install -y docker-compose
+
+git clone https://github.com/amsepahvand/telegram-sale-shop-bot.git
+
+cd telegram-sale-shop-bot
+
+sudo docker-compose up -d
+
+echo "Installation complete! The bot is now running , Enjoy it ;)"
